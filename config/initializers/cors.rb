180df55ccf6de7e15908bac589f6c6b2638b9635
Gavin Liu <du20783@gmail.com>
2020-08-27 20:32:43 +0800

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 #
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://besquevans.github.io' 
+    origins ['https://besquevans.github.io', 'localhost:5500'] 
 
     resource '*',
       headers: :any,
-      methods: [:get]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
